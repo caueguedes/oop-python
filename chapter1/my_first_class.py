ad13e22f68e755ddc58ac3b0cc4da5d38ba9fc62
caueguedes <caue.guedes91@gmail.com>
2020-09-19 00:29:23 -0300

    chapter 2 - my first class

---
+++ chapter1/my_first_class.py
@@ -0,0 +1,8 @@
+class MyFirstClass:
+    pass
+
+a = MyFirstClass()
+b = MyFirstClass()
+
+print(a) #<__main__.MyFirstClass object at 0xb7b7faec>
+print(b) #<__main__.MyFirstClass object at 0xb7b7fbac>
