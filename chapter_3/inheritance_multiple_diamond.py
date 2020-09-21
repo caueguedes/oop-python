fdb9d5690c3713c0d964b0b17d45591e60a86207
caueguedes <caue.guedes91@gmail.com>
2020-09-20 21:09:27 -0300

    chapter 3 - inheritance fix name, polymorphism

---
+++ chapter_3/inheritance_multiple_diamond.py
@@ -0,0 +1,45 @@
+class BaseClass:
+    num_base_calls = 0
+
+    def call_me(self):
+        print("Calling method on Base Class.")
+        self.num_base_calls += 1
+
+
+class LeftSubclass(BaseClass):
+    num_let_calls = 0
+
+    def call_me(self):
+        BaseClass.call_me(self)
+        print("Calling method on Left Class.")
+        self.num_let_call += 1
+
+
+class RightSubclass(BaseClass):
+    num_right_calls = 0
+
+    def call_me(self):
+        BaseClass.call_me(self)
+        print("Calling method on Right Class.")
+        self.num_right_calls += 1
+
+
+class Subclass(LeftSubclass, RightSubclass):
+    num_sub_calls = 0
+
+    def call_me(self):
+        LeftSubclass.call_me(self)
+        RightSubclass.call_me(self)
+        print("Calling method on Sub Class.")
+        self.num_sub_calls += 1
+
+
+s = Subclass()
+s.call_me()
+# Calling method on Base Class
+# Calling method on Left Subclass
+# Calling method on Base Class
+# Calling method on Right Subclass
+# Calling method on Subclass
+
+print(s.num_sub_calls, s.num_left_calls, s.num_right_calls, s.num_base_calls)  # 1 1 1 2
