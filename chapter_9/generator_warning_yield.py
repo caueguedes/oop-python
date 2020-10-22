7648b87b6fdbff971bc911fadbd37718fd1bdf40
caueguedes <caue.guedes91@gmail.com>
2020-10-22 16:15:55 -0300

    chapter 9 - generators - yield from another iterator

--- chapter_9/generator_warning_yield.py
+++ chapter_9/generator_warning_yield.py
@@ -16,3 +16,17 @@ with open(inname) as infile:
 
 
 print(warnings_filter([]))  # <generator object warnings_filter at 0xb728c6bc>
+
+
+# yield from another iterator
+def warnings_filter_from(infilename):
+    with open(infilename) as infile:
+        yield from (
+            l.replace("\tWARNING", "") for l in infile if "WARNING" in l
+        )
+
+
+filter = warnings_filter_from(inname)
+with open(outname, "w") as outfile:
+    for l in filter:
+        outfile.write(l)
